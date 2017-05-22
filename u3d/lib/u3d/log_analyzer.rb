5a04f3f955f7ac563ad819f9071b598139807913
Paul Niezborala <paulniezbo@gmail.com>
2017-05-22 12:06:12 +0200

    [WIP] JSON rules for log analyzer
    The log filter rules have been taken out of log_analyzer.rb to a
    JSON config file for easier handling and better clarity.

--- u3d/lib/u3d/log_analyzer.rb
+++ u3d/lib/u3d/log_analyzer.rb
@@ -1,21 +1,63 @@
+require 'json'
+
 module U3d
   class LogAnalyzer
-    MONO_INI = /Initialize mono/
-
+    RULES_PATH = File.expand_path('../../../config/log_rules.json', __FILE__)
     class << self
+      def load_rules
+        data = {}
+        rules = {}
+        File.open(RULES_PATH, 'r') do |f|
+          data = JSON.parse(f.read)
+        end
+        data.each do |name, r|
+          next unless r['active']
+          r['start_pattern'] = Regexp.new r['start_pattern'] if r['start_pattern']
+          r['end_pattern'] = Regexp.new r['end_pattern'] if r['end_pattern']
+          rules[name] = r
+        end
+        puts rules
+        rules
+      end
+
       def pipe(i, sleep_time: 0.0)
-        buffer = {
-          type: nil,
-          message: nil,
-          expected_pattern: nil,
-          lines: []
-        }
+        active_rule = nil
+        lines_buffer = []
+        rules = load_rules
         begin
           while true do
             select([i])
             line = i.readline
-            if line =~ MONO_INI
-              UI.message line
+            if active_rule
+              pattern = active_rule['end_pattern']
+              if line =~ pattern
+                time = line.match(pattern)[:time]
+                if active_rule['end_message']
+                  message = active_rule['end_message']
+                  message += " (#{time} seconds)" if time
+                else
+                  message = line.chomp
+                end
+                UI.message message
+                active_rule = nil
+                lines_buffer.clear
+              else
+                lines_buffer << line if active_rule['store_lines']
+              end
+            else
+              rules.values.each do |rule|
+                if line =~ rule['start_pattern']
+                  if rule['end_pattern']
+                    active_rule = rule
+                  end
+                  if rule['start_message']
+                    UI.message rule['start_message']
+                  else
+                    UI.message line.chomp
+                  end
+                  break
+                end
+              end
             end
             sleep(sleep_time)
           end
