cdcc41ae6acc1c39b4a4a9ff7a032b67f06d15c4
Jerome Lacoste <jerome.lacoste@gmail.com>
2017-07-19 22:04:29 +0200

    download/cache access: make things consistent

--- u3d/lib/u3d/commands.rb
+++ u3d/lib/u3d/commands.rb
@@ -90,7 +90,8 @@ module U3d
         if os == :linux
           UI.important 'Option -a | --all not available for Linux' if options[:all]
           UI.important 'Option -p | --packages not available for Linux' if options[:packages]
-          files << ["Unity #{version}", Downloader::LinuxDownloader.download(version, cache['linux']['versions']), {}]
+          downloader = Downloader::LinuxDownloader
+          files << ["Unity #{version}", downloader.download(version, cache[os.id2name]['versions']), {}]
         else
           downloader = Downloader::MacDownloader if os == :mac
           downloader = Downloader::WindowsDownloader if os == :win
@@ -131,6 +132,7 @@ module U3d
         if os == :linux
           UI.important 'Option -a | --all not available for Linux' if options[:all]
           UI.important 'Option -p | --packages not available for Linux' if options[:packages]
+          downloader = Downloader::LinuxDownloader
           files << ["Unity #{version}", Downloader::LinuxDownloader.local_file(version), {}]
         else
           downloader = Downloader::MacDownloader if os == :mac
