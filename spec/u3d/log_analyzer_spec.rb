f95448b2548286efa97c17a3822d50eb7c149d02
Jerome Lacoste <jerome.lacoste@gmail.com>
2017-07-24 00:42:29 +0200

    u3d/log_analyzer/specs: do not include RSpec::Matchers in file as it breaks standard expects

--- spec/u3d/log_analyzer_spec.rb
+++ spec/u3d/log_analyzer_spec.rb
@@ -23,29 +23,31 @@
 require 'u3d/log_analyzer'
 require 'json'
 
-include RSpec::Matchers
-expectations = {
-  'phase_active' => proc do |phases|
+class CustomExpectations
+  include RSpec::Matchers
+
+  def phase_active(phases)
     expect(phases).to have_key('TEST_ACTIVE')
-  end,
-  'phase_not_active' => proc do |phases|
+  end
+  def phase_not_active(phases)
     expect(phases).not_to have_key('TEST_NOT_ACTIVE')
-  end,
-  'silent_phase_use' => proc do |phases|
+  end
+  def silent_phase_use(phases)
     expect(phases).to have_key('TEST')
-  end,
-  'silent_phase_rules' => proc do |phases|
+  end
+  def silent_phase_rules(phases)
     expect(phases['TEST']['rules']).to be_empty
-  end,
-  'phase_start_pattern_parsing' => proc do |phases|
+  end
+  def phase_start_pattern_parsing(phases)
     expect(phases['TEST']['phase_start_pattern']).to eql(/This is a pattern/)
   end
-}
+end
+
 rules_data_file = File.expand_path('../../data/rules_data.json', __FILE__)
 
 describe U3d do
   describe U3d::LogAnalyzer do
-    describe '.load_rules' do
+    describe '.load_rules custom' do
       data =  JSON.parse(File.read(rules_data_file))
       data.each do |key, test_cases|
         it test_cases['message'] do
@@ -53,7 +55,7 @@ describe U3d do
           allow(File).to receive(:read) { test_cases['ruleset'].to_json }
 
           _gen, parsed_phases = U3d::LogAnalyzer.new.load_rules
-          expectations[key].call(parsed_phases)
+          CustomExpectations.new.public_send(key, parsed_phases)
         end
       end
     end
