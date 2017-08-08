df6fd6ca7ff373ba3347cd79fabb3c944dd27a30
Jerome Lacoste <jerome.lacoste@gmail.com>
2017-08-08 14:46:24 +0200

    Coveralls cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,10 +22,10 @@
 
 require 'simplecov'
 require 'coveralls'
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
-]
+])
 SimpleCov.start
 # Coveralls.wear!
 
