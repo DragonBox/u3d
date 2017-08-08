6d8be32fd18a70d6e92a362ce39e3701fbe8a55d
Jerome Lacoste <jerome.lacoste@gmail.com>
2017-08-08 16:19:13 +0200

    Rubocop Rakefile cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,10 +22,10 @@
 
 require 'simplecov'
 require 'coveralls'
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-])
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+  [SimpleCov::Formatter::HTMLFormatter,
+   Coveralls::SimpleCov::Formatter]
+)
 SimpleCov.start
 # Coveralls.wear!
 
