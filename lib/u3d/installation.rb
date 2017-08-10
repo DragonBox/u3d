9f1feaa0f9bb33ac3185000d3bfb9549c5b60870
Jerome Lacoste <jerome.lacoste@gmail.com>
2017-08-10 08:48:46 +0200

    [tech] split installer file into 3 (addds installation, unity_project) (#65)
    * cleanup: split installer file in 3 (adds installation, unity_project)
    
    * cleanup: remove long obsolete comment about mac only support :)

---
+++ lib/u3d/installation.rb
@@ -0,0 +1,167 @@
+## --- BEGIN LICENSE BLOCK ---
+# Copyright (c) 2016-present WeWantToKnow AS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+## --- END LICENSE BLOCK ---
+
+require 'u3d/utils'
+
+module U3d
+  UNITY_DIR_CHECK = /Unity_\d+\.\d+\.\d+[a-z]\d+/
+  UNITY_DIR_CHECK_LINUX = /unity-editor-\d+\.\d+\.\d+[a-z]\d+\z/
+
+  class Installation
+    def self.create(path: nil)
+      if Helper.mac?
+        MacInstallation.new path
+      elsif Helper.linux?
+        LinuxInstallation.new path
+      else
+        WindowsInstallation.new path
+      end
+    end
+  end
+
+  class MacInstallation < Installation
+    attr_reader :path
+
+    require 'plist'
+
+    def initialize(path: nil)
+      @path = path
+    end
+
+    def version
+      plist['CFBundleVersion']
+    end
+
+    def default_log_file
+      "#{ENV['HOME']}/Library/Logs/Unity/Editor.log"
+    end
+
+    def exe_path
+      "#{path}/Contents/MacOS/Unity"
+    end
+
+    def packages
+      if Utils.parse_unity_version(version)[0].to_i <= 4
+        # Unity < 5 doesn't have packages
+        return []
+      end
+      fpath = File.expand_path('../PlaybackEngines', path)
+      raise "Unity installation does not seem correct. Couldn't locate PlaybackEngines." unless Dir.exist? fpath
+      Dir.entries(fpath).select { |e| File.directory?(File.join(fpath, e)) && !(e == '.' || e == '..') }
+    end
+
+    def clean_install?
+      path =~ UNITY_DIR_CHECK
+    end
+
+    private
+
+    def plist
+      @plist ||= Plist.parse_xml("#{@path}/Contents/Info.plist")
+    end
+  end
+
+  class LinuxInstallation < Installation
+    attr_reader :path
+
+    def initialize(path: nil)
+      @path = path
+    end
+
+    def version
+      # I don't find an easy way to extract the version on Linux
+      require 'rexml/document'
+      fpath = "#{path}/Editor/Data/PlaybackEngines/LinuxStandaloneSupport/ivy.xml"
+      raise "Couldn't find file #{fpath}" unless File.exist? fpath
+      doc = REXML::Document.new(File.read(fpath))
+      version = REXML::XPath.first(doc, 'ivy-module/info/@e:unityVersion').value
+      if (m = version.match(/^(.*)x(.*)Linux$/))
+        version = "#{m[1]}#{m[2]}"
+      end
+      version
+    end
+
+    def default_log_file
+      "#{ENV['HOME']}/.config/unity3d/Editor.log"
+    end
+
+    def exe_path
+      "#{path}/Editor/Unity"
+    end
+
+    def packages
+      false
+    end
+
+    def clean_install?
+      path =~ UNITY_DIR_CHECK_LINUX
+    end
+  end
+
+  class WindowsInstallation < Installation
+    attr_reader :path
+
+    def initialize(path: nil)
+      @path = path
+    end
+
+    def version
+      require 'rexml/document'
+      fpath = "#{path}/Editor/Data/PlaybackEngines/windowsstandalonesupport/ivy.xml"
+      raise "Couldn't find file #{fpath}" unless File.exist? fpath
+      doc = REXML::Document.new(File.read(fpath))
+      version = REXML::XPath.first(doc, 'ivy-module/info/@e:unityVersion').value
+
+      version
+    end
+
+    def default_log_file
+      if @logfile.nil?
+        begin
+          loc_appdata = Utils.windows_local_appdata
+          log_dir = File.expand_path('Unity/Editor/', loc_appdata)
+          UI.important "Log directory (#{log_dir}) does not exist" unless Dir.exist? log_dir
+          @logfile = File.expand_path('Editor.log', log_dir)
+        rescue RuntimeError => ex
+          UI.error "Unable to retrieve the editor logfile: #{ex}"
+        end
+      end
+      @logfile
+    end
+
+    def exe_path
+      File.join(@path, 'Editor', 'Unity.exe')
+    end
+
+    def packages
+      # Unity prior to Unity5 did not have package
+      return [] if Utils.parse_unity_version(version)[0].to_i <= 4
+      fpath = "#{path}/Editor/Data/PlaybackEngines/"
+      raise "Unity installation does not seem correct. Couldn't locate PlaybackEngines." unless Dir.exist? fpath
+      Dir.entries(fpath).select { |e| File.directory?(File.join(fpath, e)) && !(e == '.' || e == '..') }
+    end
+
+    def clean_install?
+      path =~ UNITY_DIR_CHECK
+    end
+  end
+end
---
+++ lib/u3d/unity_project.rb
@@ -0,0 +1,45 @@
+## --- BEGIN LICENSE BLOCK ---
+# Copyright (c) 2016-present WeWantToKnow AS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+## --- END LICENSE BLOCK ---
+
+require 'u3d_core/helper'
+
+module U3d
+  class UnityProject
+    attr_reader :path
+
+    def initialize(path)
+      @path = path
+    end
+
+    def exist?
+      Dir.exist?("#{@path}/Assets") && Dir.exist?("#{@path}/ProjectSettings")
+    end
+
+    def editor_version
+      require 'yaml'
+      yaml = YAML.safe_load(File.read("#{@path}/ProjectSettings/ProjectVersion.txt"))
+      version = yaml['m_EditorVersion']
+      version.gsub!(/(\d+\.\d+\.\d+)(?:x)?(\w\d+)(?:Linux)?/, '\1\2') if Helper.linux?
+      version
+    end
+  end
+end
