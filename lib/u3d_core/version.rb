740d9ed1c86215ca83c391711407628e6213db04
Jerome Lacoste <jerome.lacoste@gmail.com>
2017-10-01 20:35:08 +0200

    u3d/all: detect ruby 2.0 usage and move checks into compatibility file

---
+++ lib/u3d_core/version.rb
@@ -0,0 +1,41 @@
+## --- BEGIN LICENSE BLOCK ---
+# Copyright (c) 2016-present WeWantToKnow AS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+## --- END LICENSE BLOCK ---
+module U3dCore
+  class Version
+    include Comparable
+    attr_reader :major, :minor, :patch
+
+    def initialize(version)
+      @major, @minor, @patch = version.split('.').map(&:to_i)
+    end
+
+    def to_a
+      [major, minor, patch].compact
+    end
+
+    def <=>(other)
+      (major <=> other.major).nonzero? ||
+        (minor <=> other.minor).nonzero? ||
+        patch <=> other.patch
+    end
+  end
+end
