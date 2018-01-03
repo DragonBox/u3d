b7fedccf6c52dcf12a887b1a8b0add234433a3b3
Jerome Lacoste <jerome.lacoste@gmail.com>
2018-01-03 10:09:57 +0100

    Bump version to 1.0.13

--- lib/u3d/version.rb
+++ lib/u3d/version.rb
@@ -21,7 +21,7 @@
 ## --- END LICENSE BLOCK ---
 
 module U3d
-  VERSION = '1.0.12'.freeze
+  VERSION = '1.0.13'.freeze
   DESCRIPTION = 'Provides numerous tools for installing, managing and running the Unity3D game engine from command line.'.freeze
   UNITY_VERSIONS_NOTE = "Unity3d uses the following version formatting: 0.0.0x0. The \'x\' can takes different values:\n"\
   "\t. 'f' are the main release candidates for Unity3d\n"\
