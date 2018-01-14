33d2dea941795bd092a16c805f0c652246fbd510
Jerome Lacoste <jerome.lacoste@gmail.com>
2018-01-14 08:17:39 +0100

    u3d/install: allow to download from one platform while on another one

--- lib/u3d/commands.rb
+++ lib/u3d/commands.rb
@@ -103,7 +103,8 @@ module U3d
       def install(args: [], options: {})
         version = specified_or_current_project_version(args[0])
 
-        os = U3dCore::Helper.operating_system
+        UI.user_error!("install can only work with current operating system. You specified #{options[:operating_system]}") if options[:install] && options[:operating_system]
+        os = valid_os_or_current(options[:operating_system])
 
         packages = packages_with_unity_first(os, options)
 
