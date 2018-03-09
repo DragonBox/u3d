6db85a2b55a8c49aaa20446c9498715ce160c4f9
Jerome Lacoste <jerome.lacoste@gmail.com>
2018-03-09 22:14:49 +0100

    Bump version to 1.0.20

--- lib/u3d/version.rb
+++ lib/u3d/version.rb
@@ -21,7 +21,7 @@
 ## --- END LICENSE BLOCK ---
 
 module U3d
-  VERSION = '1.0.19'.freeze
+  VERSION = '1.0.20'.freeze
   DESCRIPTION = 'Provides numerous tools for installing, managing and running the Unity game engine from command line.'.freeze
   UNITY_VERSIONS_NOTE = "Unity uses the following version formatting: 0.0.0x0. The \'x\' can takes different values:\n"\
   "\t. 'f' are the main release candidates for Unity\n"\
