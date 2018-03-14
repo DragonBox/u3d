9ab11b9a30e98a53975b156268a7a4eadfab3065
Paul Niezborala <paulniezbo@gmail.com>
2018-03-14 18:54:57 +0100

    Implement basic failure reporter

---
+++ lib/u3d/failure_reporter.rb
@@ -0,0 +1,32 @@
+require 'date'
+require 'fileutils'
+require 'json'
+
+module U3d
+  class FailureReporter
+    class << self
+      def report(failure_type: "DEFAULT", failure_message: "", data: {})
+        return unless ENV['U3D_REPORT_FAILURES']
+        report = {
+          type: failure_type,
+          message: failure_message,
+          data: data
+        }
+
+        FileUtils.mkdir_p default_report_path
+        report_file = File.join(
+          default_report_path,
+          "#{failure_type}.#{DateTime.now.strftime('%Y%m%dT%H%M')}.failure.json"
+        )
+
+        File.open(report_file, 'w') do |file|
+          file.write JSON.pretty_generate(report)
+        end
+      end
+
+      def default_report_path
+        File.join(U3dCore::Helper.data_path, 'failures')
+      end
+    end
+  end
+end
